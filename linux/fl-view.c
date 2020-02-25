7c61868fc3d3af742742fa2f51bcd0f50af053b2
Robert Ancell <robert.ancell@canonical.com>
2020-02-26 10:54:10 +1300

    Show an empty window

--- linux/fl-view.c
+++ linux/fl-view.c
@@ -1,17 +1,42 @@
+/*
+ * Copyright (C) 2020 Canonical Ltd.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2 or version 3 of the License.
+ * See http://www.gnu.org/copyleft/lgpl.html the full text of the license.
+ */
+
 #include "fl-view.h"
 
 typedef struct
 {
+    int placeholder;
 } FlViewPrivate;
 
 G_DEFINE_TYPE_WITH_PRIVATE (FlView, fl_view, GTK_TYPE_GL_AREA)
 
+static gboolean
+fl_view_render (GtkGLArea *widget, GdkGLContext *context)
+{
+    return FALSE;
+}
+
 static void
 fl_view_class_init (FlViewClass *klass)
 {
+    GtkGLAreaClass *gl_area_class = GTK_GL_AREA_CLASS (klass);
+
+    gl_area_class->render = fl_view_render;
 }
 
 static void
 fl_view_init (FlView *self)
 {
+}
+
+FlView *
+fl_view_new (void)
+{
+    return g_object_new (fl_view_get_type (), NULL);
 }
--- linux/main.c
+++ linux/main.c
@@ -1,5 +1,20 @@
+#include <gtk/gtk.h>
+
+#include "fl-view.h"
+
 int
 main (int argc, char **argv)
 {
+    gtk_init (&argc, &argv);
+
+    GtkWidget *window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+    gtk_widget_show (window);
+
+    FlView *view = fl_view_new ();
+    gtk_widget_show (GTK_WIDGET (view));
+    gtk_container_add (GTK_CONTAINER (window), GTK_WIDGET (view));
+
+    gtk_main ();
+
     return 0;
 }
