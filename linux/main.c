e478782c9090aca740da9deffafeffaa0023c2e1
Robert Ancell <robert.ancell@canonical.com>
2020-02-27 14:00:02 -0800

    Load icudtl.dat

--- linux/main.c
+++ linux/main.c
@@ -12,7 +12,7 @@ main (int argc, char **argv)
 
     FlView *view = fl_view_new ();
     fl_view_set_assets_path (view, "./build/flutter_assets");
-    //fl_view_set_icu_data_path (view, "./build/flutter_assets");
+    fl_view_set_icu_data_path (view, "./linux/flutter/ephemeral/icudtl.dat");
 
     gtk_widget_show (GTK_WIDGET (view));
     gtk_container_add (GTK_CONTAINER (window), GTK_WIDGET (view));
